e579eaf612e77e40a6a6a9e1c76285de07086dab
tulpia <teihotaata.alec@gmail.com>
2019-04-25 11:53:48 +0200

    ajout des fichiers webpack dans le header et footer, ajout des fontes et variables

--- public/view/template.php
+++ public/view/template.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../dist/styles.css">
     <!--Variable titre de la page-->
     <title><?= $title ?></title>
 </head>
 <body>
     <!--Variable contenu du Body-->
-    <?= $content ?>
+    <main class="wrapper">
+        <?= $content ?>
+    </main>
+    <script src="../dist/main.js"></script>
 </body>
 </html>
