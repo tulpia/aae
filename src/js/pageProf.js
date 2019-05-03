5c0d880795604b1e8c8d666124a4df2f19e7cb3b
tulpia <teihotaata.alec@gmail.com>
2019-05-03 14:01:36 +0200

    modifs de la fonction createExo pour le filtre resultats

--- src/js/pageProf.js
+++ src/js/pageProf.js
@@ -101,14 +101,121 @@ document.addEventListener('DOMContentLoaded', () => {
     // creation des details - classe
     const createDetailsClasse = document.createElement('span');
     createDetailsClasse.className = 'details__matiere';
-    createDetailsClasse.innerText = `, ${content.classe}`;
+    createDetailsClasse.innerText = `, ${content.niveau}`;
     createDetailsMatiere.appendChild(createDetailsClasse);
 
     // creation des details - date
     const createDetailsDate = document.createElement('p');
     createDetailsDate.className = 'details__date';
+    createDetailsDate.innerText = content.dateCrea;
+    createDetailsContainer.appendChild(createDetailsDate);
+
+    finalContainer.appendChild(createContainer);
+  };
+
+  const createExoResultat = (content, finalContainer) => {
+    // creation du container
+    const createContainer = document.createElement('div');
+    createContainer.className = 'question-container';
+
+    // check si c'est une archive$
+    if (content.isArchive === 1) {
+      createContainer.classList.add('is-archive')
+    }
+
+    // creation du conteneur du titre
+    const createTitleContainer = document.createElement('div');
+    createTitleContainer.className = 'questions-container__title-container';
+    createContainer.appendChild(createTitleContainer);
+
+    // creation du titre
+    const createTitle = document.createElement('p');
+    createTitle.className = 'question-container__title';
+    createTitle.innerText = content.titre;
+    createTitleContainer.appendChild(createTitle);
+
+    // creation du formulaire
+    const createForm = document.createElement('form');
+    createForm.className = 'questions-container__btn-editer';
+    createForm.setAttribute('method', 'post');
+    createForm.setAttribute('action', 'index.php');
+    createTitleContainer.appendChild(createForm);
+
+    // creation des inputs hidden
+    const createHiddenInput1 = document.createElement('input');
+    createHiddenInput1.setAttribute('type', 'hidden');
+    createHiddenInput1.setAttribute('name', 'action');
+    createHiddenInput1.setAttribute('value', 'show_resultatDetail');
+    createForm.appendChild(createHiddenInput1);
+
+    const createHiddenInput2 = document.createElement('input');
+    createHiddenInput2.setAttribute('type', 'hidden');
+    createHiddenInput2.setAttribute('name', 'idResultat');
+    createHiddenInput2.setAttribute('value', content.id);
+    createForm.appendChild(createHiddenInput2);
+
+    // creation du bouton fleche
+    const createLabelFleche = document.createElement('label');
+    createLabelFleche.className = 'btn-editer';
+    createForm.appendChild(createLabelFleche);
+
+    const createLabelInput = document.createElement('input');
+    createLabelInput.setAttribute('type', 'submit');
+    createLabelInput.className = 'btn-editer-text';
+    createLabelFleche.appendChild(createLabelInput);
+
+    // creation des details - container
+    const createDetailsContainer = document.createElement('div');
+    createDetailsContainer.className = 'question-container__details';
+    createContainer.appendChild(createDetailsContainer);
+
+    // creation des details - date
+    const createDetailsDate = document.createElement('p');
+    createDetailsDate.className = 'details__matiere';
     createDetailsDate.innerText = content.dateAccessible;
     createDetailsContainer.appendChild(createDetailsDate);
+
+    // creation des details - matiere
+    const createDetailsMatiere = document.createElement('span');
+    createDetailsMatiere.className = 'details__matiere';
+    createDetailsMatiere.innerText = `, ${content.matiere}`;
+    createDetailsDate.appendChild(createDetailsMatiere);
+
+    // creation des details - classe
+    const createDetailsClasse = document.createElement('p');
+    createDetailsClasse.className = 'details__matiere';
+    createDetailsClasse.innerText = content.classe;
+    createDetailsContainer.appendChild(createDetailsClasse);
+
+    // creation des details - classe - classeNom
+    if (content.ClasseNom) {
+      const createDetailsClasseNom = document.createElement('span');
+      createDetailsClasseNom.className = 'details__matiere';
+      createDetailsClasseNom.innerText = ` ${content.ClasseNom}`;
+      createDetailsClasse.appendChild(createDetailsClasseNom);
+    }
+
+    // creation des details - classe - classeNom - optionCours
+    if (content.optionCours !== null) {
+      const createDetailsoptionCours = document.createElement('span');
+      createDetailsoptionCours.className = 'details__matiere';
+      createDetailsoptionCours.innerText = ` ${content.optionCours}`;
+      createDetailsClasse.appendChild(createDetailsoptionCours);
+    }
+
+    // creation des reponses - container
+    const createReponses = document.createElement('p');
+    createReponses.className = 'details__matiere';
+    createReponses.innerText = `Réponses : ${content.nbRepondu} / ${content.nbAutoEval}`;
+    createDetailsContainer.appendChild(createReponses);
+
+    // creation des reponses - container
+    if (content.dateDerReponse !== null) {
+      const createLastReponse = document.createElement('p');
+      createLastReponse.className = 'details__matiere';
+      createLastReponse.innerText = `Dernière réponse le : ${content.dateDerReponse}`;
+      createDetailsContainer.appendChild(createLastReponse);
+    }
 
     finalContainer.appendChild(createContainer);
   };
@@ -117,16 +224,16 @@ document.addEventListener('DOMContentLoaded', () => {
   if (formulaireQuestionnaires) {
     const formulaireLoading = document.querySelector('.loading-container--questionnaires');
 
-    formulaireQuestionnaires.addEventListener('submit', function(e) {
+    formulaireQuestionnaires.addEventListener('submit', function (e) {
       e.preventDefault();
       formulaireLoading.classList.add('active');
 
       formData = new FormData(formulaireQuestionnaires);
       axios({
-        method: 'post',
-        url: formulaireQuestionnaires.getAttribute('action'),
-        data: formData
-      })
+          method: 'post',
+          url: formulaireQuestionnaires.getAttribute('action'),
+          data: formData
+        })
         .then(response => {
           if (formulaireLoading.classList.contains('active')) {
             formulaireLoading.classList.remove('active');
@@ -138,7 +245,6 @@ document.addEventListener('DOMContentLoaded', () => {
               createExo(reponse, formulaireQuestionnairesResultContainer);
             }
           }
-          console.log(response);
         })
         .catch(() => {
           console.log('fatal error');
@@ -150,16 +256,16 @@ document.addEventListener('DOMContentLoaded', () => {
   if (formulaire) {
     const formulaireLoading = document.querySelector('.loading-container--resultats');
 
-    formulaire.addEventListener('submit', function(e) {
+    formulaire.addEventListener('submit', function (e) {
       e.preventDefault();
       formulaireLoading.classList.add('active');
 
       formData = new FormData(formulaire);
       axios({
-        method: 'post',
-        url: formulaire.getAttribute('action'),
-        data: formData
-      })
+          method: 'post',
+          url: formulaire.getAttribute('action'),
+          data: formData
+        })
         .then(response => {
           if (formulaireLoading.classList.contains('active')) {
             formulaireLoading.classList.remove('active');
@@ -168,7 +274,7 @@ document.addEventListener('DOMContentLoaded', () => {
             formulaireResultContainer.innerText = '';
             for (const reponse of response.data.items) {
               console.log(reponse);
-              createExo(reponse, formulaireResultContainer);
+              createExoResultat(reponse, formulaireResultContainer);
             }
           }
           console.log(response);
