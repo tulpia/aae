dea8586874d40b0f218c92b03189244386a9e014
Laure Cohen <laure.chn@gmail.com>
2019-05-17 10:45:24 +0200

    AAE-08: Theming page réponse questions + radio graduate smiley

--- public/view/autoevalQuestionsEleveView.php
+++ public/view/autoevalQuestionsEleveView.php
@@ -19,7 +19,7 @@ require('view/headerUserView.php');
         <input type="hidden" name="action" value="do_ReponseEleveEnregistrer">
         <input type="hidden" name="idAutoEval" value="<?= $idAutoEval ?>">
 
-        <table>
+        <table class="smiley-table">
 
 
             <?php
@@ -31,12 +31,32 @@ require('view/headerUserView.php');
                 ?>
 
                 <tr>
-                    <td><?= $quantieme . " - " . $libelle ?></td>
-                    <td><label><input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="30">:D</label></td>
-                    <td><label><input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="20">:)</label></td>
-                    <td><label><input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="10">:|</label></td>
-                    <td><label><input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="0">:(</label></td>
+                    <th><?= $question['quantieme'] ?></th>
+                    <td><?= $question['libelle'] ?></td>
 
+                    <td class="radio-wrapper clear">
+                        <label>
+                            <input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="30">
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36"><defs><style>.a{fill:none;}.b{fill:#91cbfc;}</style></defs><path class="a" d="M0,0H36V36H0Z"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(22 12.571)"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(11 12.571)"/><path class="b" d="M17.984,2A16,16,0,1,0,34,18,15.992,15.992,0,0,0,17.984,2ZM18,30.8A12.8,12.8,0,1,1,30.8,18,12.8,12.8,0,0,1,18,30.8Zm-8-9.6c1.248,3.744,4.352,6.4,8,6.4s6.752-2.656,8-6.4Z"/></svg></span>
+                        </label>
+                    </td>
+                    <td class="radio-wrapper">
+                        <label><input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="20"> 
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36"><defs><style>.a{fill:none;}.b{fill:#91cbfc;}</style></defs><g transform="translate(0)"><path class="a" d="M0,0H36V36H0Z"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(22 12.571)"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(11 12.571)"/><path class="b" d="M17.984,2A16,16,0,1,0,34,18,15.992,15.992,0,0,0,17.984,2ZM18,30.8A12.8,12.8,0,1,1,30.8,18,12.8,12.8,0,0,1,18,30.8Zm0-4a8.792,8.792,0,0,0,8.192-5.6H23.52a6.361,6.361,0,0,1-11.04,0H9.808A8.792,8.792,0,0,0,18,26.8Z"/></g></svg></span>
+                        </label>
+                    </td>
+                    <td class="radio-wrapper">
+                        <label>
+                            <input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="10">
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36"><defs><style>.a{fill:none;}.b{fill:#91cbfc;}</style></defs><g transform="translate(0)"><path class="a" d="M0,0H36V36H0Z"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(22 12.571)"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(11 12.571)"/><path class="b" d="M17.984,2A16,16,0,1,0,34,18,15.992,15.992,0,0,0,17.984,2ZM18,30.8A12.8,12.8,0,1,1,30.8,18,12.8,12.8,0,0,1,18,30.8Zm0-5.6a6.151,6.151,0,0,1,3.152.848c.192-.224,1.376-1.568,1.616-1.824A8.623,8.623,0,0,0,18,22.8a8.837,8.837,0,0,0-4.784,1.408c1.552,1.744.016.032,1.616,1.824A6.4,6.4,0,0,1,18,25.2Z"/></g></svg></span>
+                        </label>
+                    </td>
+                    <td class="radio-wrapper">
+                        <label>
+                            <input type="radio" required name="arrayIdReponse[<?= $idQuestion ?>]" value="0">
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36"><defs><style>.a{fill:none;}.b{fill:#91cbfc;}</style></defs><path class="a" d="M0,0H36V36H0Z"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(22 12.571)"/><circle class="b" cx="1.5" cy="1.5" r="1.5" transform="translate(11 12.571)"/><path class="b" d="M17.984,2A16,16,0,1,0,34,18,15.992,15.992,0,0,0,17.984,2ZM18,30.8A12.8,12.8,0,1,1,30.8,18,12.8,12.8,0,0,1,18,30.8Zm0-9.6a8.792,8.792,0,0,0-8.192,5.6H12.48a6.361,6.361,0,0,1,11.04,0h2.672A8.792,8.792,0,0,0,18,21.2Z"/></svg></span>
+                        </label>
+                    </td>
                 </tr>
             <?php
         }
@@ -58,8 +78,10 @@ require('view/headerUserView.php');
         }
         ?>
         </div>
-
-        <input type="submit" value="Valider">
+        <label class="btn btn-detail">
+            <input type="submit" value="Valider">
+        </label>
+        
 
     </form>
 
