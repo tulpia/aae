2ee95aac208cdc69f5736cc7b3c1914e9fc86c03
herr-turbo <david.klein.fr@protonmail.com>
2019-05-24 09:35:20 +0200

    passage du message.php en h2

--- public/view/message.php
+++ public/view/message.php
@@ -1,5 +1,5 @@
 <?php
     //Affiche message d'erreur s'il y en a un
     if(isset($message) && trim($message) != "" ){
-        echo('<br><br><h3>' . $message . '</h3><br><br>');
+        echo('<br><br><h2>' . $message . '</h2><br>');
     }
