abcb6bde5b8b47735eba44374c541b7b3a1beaf1
Laure Cohen <laure.chn@gmail.com>
2019-06-14 16:22:35 +0200

    Mentions légales du footer

--- public/view/footer-general.php
+++ public/view/footer-general.php
@@ -1,3 +1,3 @@
 <footer class="block-footer">
-    <a href="mentions.php">Mentions Légales</a>
+    <a href="mentions.php">Copyright © 2019 ADGL. Tous droits réservés. </a><!-- "Alec David Geoffrey Laure", en attendant mieux -->
 </footer>
